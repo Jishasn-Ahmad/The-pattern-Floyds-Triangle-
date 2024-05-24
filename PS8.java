e95c3122716a89e65dcfc71c29556105dc701954
Jishan Ahmad <139096572+Jishasn-Ahmad@users.noreply.github.com>
2024-05-24 16:26:27 +0530

    Add files via upload

---
+++ PS8.java
@@ -0,0 +1,19 @@
+public class PS8 {
+    public static void main(String[] args) {
+        //floyds triangle
+
+        int n=5;
+        int number =1;
+
+        ///outter loop
+
+        for(int i=1; i<=n; i++){
+            //inner loop
+            for(int j=1; j<=i; j++){
+                System.out.print(number+ " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
